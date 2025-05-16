81b20e3361c36d42f51175db8fa3003a2c79e184
wacaonikankan <lijianyuan0506@gmail.com>
2025-05-17 01:01:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import ee
-from google.oauth2 import service_account
+from google.auth2 import service_account
 import geemap.foliumap as geemap
 
 # 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
-service_account_info = st.secrets["GEE SERVICE ACCOUNT"]
+service_account_info = st.secrets["GEE_SERVICE_ACCOUNT"]
 
 # 使用 google-auth 進行 GEE 授權
 credentials = service_account.Credentials.from_service_account_info(

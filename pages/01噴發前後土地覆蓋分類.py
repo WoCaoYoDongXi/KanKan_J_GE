9385250efeaf93312586deabcfc2745bc75b0f8a
wacaonikankan <lijianyuan0506@gmail.com>
2025-06-07 00:57:45 +0800

    Update 01噴發前後土地覆蓋分類.py

--- pages/01噴發前後土地覆蓋分類.py
+++ pages/01噴發前後土地覆蓋分類.py
@@ -28,7 +28,7 @@ classVis = {
 my_img = (
     ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
     .filterBounds(my_point)
-    .filterDate('2021-10-01', '2021-11-30')
+    .filterDate('2021-06-01', '2021-08-31')
     .sort('CLOUDY_PIXEL_PERCENTAGE')
     .first()
     .select('B.*')
@@ -61,22 +61,21 @@ my_trainedClassifier = ee.Classifier.smileCart().train(**{
 my_newimg01 = (
     ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
     .filterBounds(my_point)
-    .filterDate('2021-12-01', '2022-01-05')
+    .filterDate('2021-11-01', '2021-12-15')  # ✅ 改為乾季尾聲
     .sort('CLOUDY_PIXEL_PERCENTAGE')
     .first()
     .select('B.*')
 )
-my_newimgClassified01 = my_newimg01.classify(my_trainedClassifier)
 
+# 噴發後（推薦2022年5月 ~ 6月）
 my_newimg02 = (
     ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
     .filterBounds(my_point)
-    .filterDate('2022-04-01', '2022-06-05')
+    .filterDate('2022-05-01', '2022-06-30')  # ✅ 改為乾季初期
     .sort('CLOUDY_PIXEL_PERCENTAGE')
     .first()
     .select('B.*')
 )
-my_newimgClassified02 = my_newimg02.classify(my_trainedClassifier)
 
 Map = geemap.Map()
 Map.centerObject(my_point, 11)

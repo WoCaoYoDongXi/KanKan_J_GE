4ce8a8460e3d4f661f9ce5f7f12db4aea52b8dac
wacaonikankan <lijianyuan0506@gmail.com>
2025-06-07 02:49:37 +0800

    Update 01噴發前後甲色影像.py

--- pages/01噴發前後甲色影像.py
+++ pages/01噴發前後甲色影像.py
@@ -4,23 +4,28 @@ from google.oauth2 import service_account
 import geemap.foliumap as geemap
 
 my_Map = geemap.Map()
-my_Map
 
 my_point = ee.Geometry.Point([-175.2049470, -21.1988048])
 
 my_img = (
     ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
     .filterBounds(my_point)
-    .filterDate('2021-10-26', '2021-10-31')
+    .filterDate('2021-09-16', '2021-09-20')
     .sort('CLOUDY_PIXEL_PERCENTAGE')#排序含雲量後取第一張含雲量最低的
     .first()
     .select('B.*')
 )
-
+my_img02 = (
+    ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
+    .filterBounds(my_point)
+    .filterDate('2022-04-21', '2022-04-25')
+    .sort('CLOUDY_PIXEL_PERCENTAGE')#排序含雲量後取第一張含雲量最低的
+    .first()
+    .select('B.*')
 
 vis_params = {'min':100, 'max': 3500, 'bands': ['B11',  'B8',  'B3']}
 
 my_Map.centerObject(my_point, 11)
 my_Map.addLayer(my_img, vis_params, "Sentinel-2")
-
+my_Map.addLayer(my_img02, vis_params, "Sentinel-2")
 my_Map.to_streamlit(height=600)

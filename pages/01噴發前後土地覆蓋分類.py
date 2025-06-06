fb8321055ce5240a781e5756d32a56389798ffb7
wacaonikankan <lijianyuan0506@gmail.com>
2025-06-07 00:53:06 +0800

    Update 01噴發前後土地覆蓋分類.py

--- pages/01噴發前後土地覆蓋分類.py
+++ pages/01噴發前後土地覆蓋分類.py
@@ -28,7 +28,7 @@ classVis = {
 my_img = (
     ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
     .filterBounds(my_point)
-    .filterDate('2021-06-01', '2021-08-31')
+    .filterDate('2021-10-01', '2021-11-30')
     .sort('CLOUDY_PIXEL_PERCENTAGE')
     .first()
     .select('B.*')

fbcc6c10e737851a4d12fc2f5d7d00cd558a6a2b
wacaonikankan <lijianyuan0506@gmail.com>
2025-06-07 03:05:16 +0800

    Update 01噴發前後假色影像.py

--- pages/01噴發前後假色影像.py
+++ pages/01噴發前後假色影像.py
@@ -4,9 +4,10 @@ from google.oauth2 import service_account
 import geemap.foliumap as geemap
 
 
-
+st.write("## 噴發日期選擇以火山噴發前後Tongatapu島上空含雲量最少之時而定")
+st.write("## 噴發前：21/09/16-20;噴發後：22/04/21-25")
 my_point = ee.Geometry.Point([-175.2049470, -21.1988048])
-
+st.write("## 火山噴發前")
 my_img = (
     ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
     .filterBounds(my_point)
@@ -20,7 +21,7 @@ m1 = geemap.Map()
 m1.centerObject(my_point, 11)
 m1.addLayer(my_img, vis_params, "噴發前")
 m1.to_streamlit(height=600)
-
+st.write("## 火山噴發後")
 my_img02 = (
     ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
     .filterBounds(my_point)

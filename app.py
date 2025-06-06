cb42a4291502232f62c780cfe2832ce50987f1d5
wacaonikankan <lijianyuan0506@gmail.com>
2025-06-06 17:52:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ st.title("🌋 東加火山災後地表變化分析")
 
 st.markdown(
     """
-    <video autoplay muted loop width="700">
+    <video autoplay muted loop controls width="720">
         <source src="eruption.mp4" type="video/mp4">
-        您的瀏覽器不支援 HTML5 影片播放。
+        您的瀏覽器不支援影片播放。
     </video>
     """,
     unsafe_allow_html=True
 )
+
 st.markdown("#### 研究動機")
 st.write("""
 當初在新聞上看見東加火山噴發的一瞬間，蕈狀雲將空照圖整個填滿，令我印象深刻。

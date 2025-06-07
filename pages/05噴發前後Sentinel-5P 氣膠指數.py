eccc6ea96ca07b7dcd6bdffa1669517dfb23b78a
wacaonikankan <lijianyuan0506@gmail.com>
2025-06-08 04:10:07 +0800

    Update 05噴發前後Sentinel-5P 氣膠指數.py

--- pages/05噴發前後Sentinel-5P 氣膠指數.py
+++ pages/05噴發前後Sentinel-5P 氣膠指數.py
@@ -61,10 +61,10 @@ m1.to_streamlit(height=400)
 # Map 2
 m2 = geemap.Map(center=[lat, lon], zoom=10)
 m2.addLayer(img2, vis_params, "噴發後")
-m1.add_legend(
-    title="S5P 吸收性氣溶膠指數 (AAI)",
+m2.add_legend(
+    title="S5P 吸收性氣膠指數 (AAI)",
     labels=["0", "0.4", "0.8", "1.2", "1.6", "2+"],
-    colors=["white", "purple", "blue", "green", "yellow", "red"]
+    colors=legend_colors_hex
 )
 st.subheader("噴發後")
 m2.to_streamlit(height=400)
